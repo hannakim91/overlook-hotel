de39fdf8356304c601c6af3ad170802bb61d6cc5
Hanna Kim <hannakim91@gmail.com>
2020-07-30 13:36:29 -0600

    Add modal template for log-in feature and change basic details

--- src/index.js
+++ src/index.js
@@ -3,8 +3,28 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+// probably don't neeed to import partial file to index.js if you're importing other scss into base.s
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+console.log('YOU CAN DO IT!!');
+
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".trigger");
+var closeButton = document.querySelector(".close-button");
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+  event.preventDefault()
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
