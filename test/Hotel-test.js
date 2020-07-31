1d1994a5f21c71ebf014c0c369717356bddba1b7
Hanna Kim <hannakim91@gmail.com>
2020-07-31 11:47:53 -0600

    Add to tests to show results for dates with no bookings

--- test/Hotel-test.js
+++ test/Hotel-test.js
@@ -21,19 +21,23 @@ describe('Hotel details', function() {
   });
   it('should calculate how many rooms are available for a given date', function() {
     const numberOfRooms = hotel.findRoomsAvailable('2020/04/22', sampleBookings)
-    const numberRooms = hotel.findRoomsAvailable('2021/01/05', sampleBookings)
+    const numberRooms = hotel.findRoomsAvailable('2025/01/05', sampleBookings)
     console.log(numberRooms)
     expect(numberOfRooms).to.equal(23);
     expect(numberRooms).to.equal(25)
   });
   it('should calculate total revenue for today\'s date', function() {
     const todaysRevenue = hotel.calculateTodaysRevenue('2020/04/22', sampleBookings, sampleRooms)
+    const nonExistentRevenue = hotel.calculateTodaysRevenue('2025/01/05', sampleBookings)
     //2 rooms - 294.56+176.36
     expect(todaysRevenue).to.equal(470.92)
+    expect(nonExistentRevenue).to.equal(0)
   });
   it('should calculate percentage of rooms occupied for today\'s date', function() {
     const percentBooked = hotel.calculatePercentBooked('2020/04/22', sampleBookings)
+    const noBookingsDate = hotel.calculatePercentBooked('2025/01/05', sampleBookings)
 
     expect(percentBooked).to.equal(8)
+    expect(noBookingsDate).to.equal(0)
   });
 });
